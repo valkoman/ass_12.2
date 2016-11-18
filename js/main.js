4f7e9cc8db419fd10e7119075493aca1ac667190
valkoman <j.valko@mcad.edu>
2016-11-18 14:11:21 -0600

    Redo clean-up

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var navButton =
+document.querySelector('.nav-icon');
+
+navButton.addEventListener('click',
+	function() {
+		console.log('clicky!!!');
+		document
+			.querySelector('body')
+			.classList
+			.toggle('nav-open');
+});
+
+var closeButton =
+document.querySelector('.close-icon')
+
+closeButton.addEventListener('click',
+	function() {
+		console.log ('clicky!!!');
+		document.
+			querySelector('body')
+			.classList
+			.toggle('nav-open');
+		});
